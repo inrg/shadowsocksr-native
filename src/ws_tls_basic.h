0dfab893d21641b2093017ab905f7f000065aa05
ssrlive <ssrlivebox@gmail.com>
2019-06-08 02:02:56 +0800

    m

--- src/ws_tls_basic.h
+++ src/ws_tls_basic.h
@@ -7,7 +7,7 @@
     "HTTP/1.1 101 Switching Protocols\r\n"                                      \
     "Upgrade: websocket\r\n"                                                    \
     "Connection: Upgrade\r\n"                                                   \
-    "Sec-WebSocket-Accept: %d\r\n"                                              \
+    "Sec-WebSocket-Accept: %s\r\n"                                              \
     "\r\n"
 
 // Sec-WebSocket-Accept = 
